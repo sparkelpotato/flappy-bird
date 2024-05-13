3e6f7deede58420819800ce898d6445dc8350e3a
ryan <ryan.grindstaff@stu.pfisd.net>
2024-05-13 19:33:21 +0000

    Update 'FlappyWorld.java'

--- FlappyWorld.java
+++ FlappyWorld.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class FlappyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Ryan
+ * @version finished
  */
 public class FlappyWorld extends World
 {
