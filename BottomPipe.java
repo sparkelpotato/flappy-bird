3f9c8dde4e3a6e09288d9748ede1110fa8f133ac
ryan <ryan.grindstaff@stu.pfisd.net>
2024-05-16 14:36:10 +0000

    Update 'BottomPipe.java'

--- BottomPipe.java
+++ BottomPipe.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class BottomPipe here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author ryan
+ * @version final
  */
 public class BottomPipe extends Pipe
 {
